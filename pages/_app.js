765fcb70cb8893cd2910ae938637f343ead56db2
kornatskyi <bohdan.kornatskyi@gmail.com>
2021-07-06 17:40:28 -0700

    Set up theme and landing page

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,43 @@
-import '../styles/globals.css'
+import React, { useContext } from 'react';
+import PropTypes from 'prop-types';
+import Head from 'next/head';
+import { MuiThemeProvider } from '@material-ui/core';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeContext, ThemeProvider } from '../src/theme';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function ThemeConsumer({ Component, pageProps }) {
+  const { theme } = useContext(ThemeContext)
+  return <MuiThemeProvider theme={theme}>
+    {/* CssBaseline sets default style values like a box-sizing etc. */}
+    <CssBaseline />
+    <Component {...pageProps} />
+  </MuiThemeProvider>
 }
 
-export default MyApp
+export default function MyApp(props) {
+
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <React.Fragment>
+      <Head>
+        <title>Bohdan Kornatskyi</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider>
+        <ThemeConsumer {...props} />
+      </ThemeProvider>
+    </React.Fragment>
+  );
+}
+
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired,
+};
